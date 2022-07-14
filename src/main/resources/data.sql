ea39b7c6cd4de05a88587abdf45243df73b039b5
ThaisAssuncao <thais.ap.assuncao12@gmail.com>
2022-07-14 20:13:59 -0300

    feature(transacao):Criacao do endpoint de saldo (#6)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
 insert into tb_usuario(id, created_by, creation_date_time, last_modified_by, last_modified_date, version, papel, senha, username)
 values
-(1,"engc.thales@gmail.com", NOW(), "engc.thales@gmail.com", NOW(), 0, "ROLE_CORRENISTA", "$10$AVGA6EPiQ1L9L/1EC9AzbOFmN1v3MCon03doZVOwHrIR1nW7guGS.", "1234");
+(1,"engc.thales@gmail.com", NOW(), "engc.thales@gmail.com", NOW(), 0, "ROLE_CORRENTISTA", "$10$AVGA6EPiQ1L9L/1EC9AzbOFmN1v3MCon03doZVOwHrIR1nW7guGS.", "1234");
